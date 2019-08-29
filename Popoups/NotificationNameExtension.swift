9c34a9956ae154c5c6a7781f7773aaed11308a5d
Iurii Popov <30910230+titanium901@users.noreply.github.com>
2019-08-29 19:04:31 +0300

    Add files via upload

---
+++ Popoups/NotificationNameExtension.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationNameExtension.swift
+//  Popoups
+//
+//  Created by Yury Popov on 29/08/2019.
+//  Copyright © 2019 Yury Popov. All rights reserved.
+//
+
+import Foundation
+
+
+extension Notification.Name {
+    static let saveDataTime = Notification.Name("saveDateTime")
+}
---
+++ Popoups/PopoupDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  PopoupDelegate.swift
+//  Popoups
+//
+//  Created by Yury Popov on 29/08/2019.
+//  Copyright © 2019 Yury Popov. All rights reserved.
+//
+
+import Foundation
+
+protocol PopoupDelegate {
+    func popoupValueSelected(value: String)
+}
